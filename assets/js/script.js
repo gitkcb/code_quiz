5e0b3ad96c2e5dd97760e0d9242811ac0e3babb5
gitkcb <kcharlesbell@gmail.com>
2022-10-07 11:16:07 -0400

    worked on saving initials to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,15 +13,21 @@ var answer4= fourth.querySelector('ul');
 var fifth= document.querySelector('.fifth');
 var answer5= fifth.querySelector('ul');
 var enterhighscores = document.querySelector('.enterhighscores');
-var alltime = document.querySelector('.alltime');
-var incorrect= document.querySelector('.incorrect');
+var finalscore = enterhighscores.querySelector('h2');
+var alltime = document.querySelector('#alltime');
+var incorrect= document.querySelectorAll('.incorrect');
 var submit = document.querySelector('.submit');
+var retbut= document.querySelector('.return');
+var initials= document.querySelector('#textarea')
 
 
 var secondsLeft = 70;
 function startTimer(){
     welcome.style.display ="none";
     first.style.display ='block';
+    timerInterval;
+    
+}
  
 var timerInterval = setInterval(function(){
     secondsLeft--;
@@ -29,7 +35,7 @@ var timerInterval = setInterval(function(){
     if (secondsLeft===0){
         clearInterval(timerInterval);
         loss();
-
+        
     }
 function loss (){
     main.innerHTML ='You ran out of time!'
@@ -39,14 +45,14 @@ function timeLoss(){
     
     secondsLeft -=10;
 }
+function myStopFunction() {
+    clearInterval(timerInterval);
 
 incorrect.onclick = timeLoss;
 
 }
-
-
-
 button.onclick = startTimer;
+2
 function nextQuestion(){
     first.style.display="none";
     second.style.display="block";
@@ -71,19 +77,53 @@ let score;
 function nextQuestion4(){
     fifth.style.display="none";
     enterhighscores.style.display="block";
-    let score = secondsLeft
-
+    let score = secondsLeft;
+    myStopFunction();
+    finalscore.innerHTML= "Your final score is: " + score;
     console.log(score);  
-    
+}
+answer5.onclick = nextQuestion4;
+function saveDetails(){
+    var highDetails = {
+ highInitials:initials.value.trim(),
+
+    }
+localStorage.setItem('highDetails', JSON.stringify(highDetails))
+};
+function renderDetails(){
+    var lastGame = JSON.parse(localStorage.getItem("highDetails"));
+    if (lastGame !== null) {
+        document.getElementById("saved-initials").innerHTML = lastGame + '-' + score;
+       
+       
+
+    }
+        else{
+            return;
+        }
+        console.log( lastGame + score);
+
 }
 
-answer5.onclick = nextQuestion4;
+
 function scoreboard(){
     enterhighscores.style.display='none';
-    alltime.style.display = 'block';
+    alltime.style.display='block';
+    saveDetails();
+    renderDetails();
+    
+    
 
 }
 submit.onclick = scoreboard;
+function homescreen(){
+  enterhighscores.style.display='none';
+  main.style.display='block';
+  
+}
+retbut.onclick = homescreen;
+
+
 
 
 
