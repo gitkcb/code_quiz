16c676019ba23209d90c5bee66ed2258ed55e0c6
gitkcb <kcharlesbell@gmail.com>
2022-10-11 12:32:16 -0400

    time reduced on wrong answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,82 +18,103 @@ var alltime = document.querySelector('#alltime');
 var incorrect= document.querySelectorAll('.incorrect');
 var submit = document.querySelector('.submit');
 var retbut= document.querySelector('.return');
-var initials= document.querySelector('#textarea')
+var initials= document.querySelector('#textarea');
+var firincorrect=first.querySelector('.incorrect')
+var firincorrect1=first.querySelector('.incorrect1')
+var firincorrect2=first.querySelector('.incorrect2')
+var secincorrect= second.querySelector('.incorrect')
+var secincorrect1=second.querySelector('.incorrect1')
+var secincorrect2=second.querySelector('.incorrect2')
+var thiincorrect= third.querySelector('.incorrect')
+var thiincorrect1=third.querySelector('.incorrect1')
+var thiincorrect2=third.querySelector('.incorrect2')
+var fouincorrect= fourth.querySelector('.incorrect')
+var fouincorrect1=fourth.querySelector('.incorrect1')
+var fouincorrect2=fourth.querySelector('.incorrect2')
+var fifincorrect= fifth.querySelector('.incorrect')
+var fifincorrect1=fifth.querySelector('.incorrect1')
+var fifincorrect2=fifth.querySelector('.incorrect2')
 
-
+var timerInterval;
 var secondsLeft = 70;
 function startTimer(){
     welcome.style.display ="none";
-    first.style.display ='block';}
-    
-    
-
- 
-var timerInterval = setInterval(function(){
+    first.style.display ='block'; 
+    timerInterval = setInterval(function(){
     
     secondsLeft--;
     timeEl.textContent= "Time:" + secondsLeft;
-    if (secondsLeft===0){
-        clearInterval(timerInterval);
-        loss();
-        
-    }
-function loss (){
-    main.innerHTML ='You ran out of time!'
-}
-},1000)
-function timeLoss(){
-    
-    secondsLeft -=10;}
-
+    },1000)}
+function startQuiz(){
+    startTimer(); } 
 function myStopFunction() {
+    if (secondsLeft===0){
     clearInterval(timerInterval);
-
-
-
+    main.innerHTML ='You ran out of time!';
+   }}
+   myStopFunction();
+function timeLoss(){
+    secondsLeft -= 10;
 }
-incorrect.onclick = timeLoss;
 
-button.onclick = startTimer;
-2
+button.addEventListener("click",startQuiz);
+
 function nextQuestion(){
     first.style.display="none";
     second.style.display="block";
-  }
+ }
 answer1.onclick = nextQuestion;
+firincorrect.onclick= timeLoss;
+firincorrect1.onclick= timeLoss;
+firincorrect2.onclick= timeLoss;
 function nextQuestion1(){
     second.style.display="none";
     third.style.display="block";
+  
 }
 answer2.onclick = nextQuestion1;
+secincorrect.onclick = timeLoss;
+secincorrect1.onclick= timeLoss;
+secincorrect2.onclick= timeLoss;
 function nextQuestion2(){
     third.style.display="none";
     fourth.style.display="block";
 }
 answer3.onclick = nextQuestion2;
+thiincorrect.onclick= timeLoss;
+thiincorrect1.onclick= timeLoss;
+thiincorrect2.onclick= timeLoss;
 function nextQuestion3(){
     fourth.style.display="none";
     fifth.style.display="block";
+   
 }
 answer4.onclick = nextQuestion3;
+fouincorrect.onclick= timeLoss;
+fouincorrect1.onclick= timeLoss;
+fouincorrect2.onclick= timeLoss;
 let score;
 function nextQuestion4(){
     fifth.style.display="none";
     enterhighscores.style.display="block";
     let score = secondsLeft;
-    myStopFunction();
+   
+    
     finalscore.innerHTML= "Your final score is: " + score;
     console.log(score);  
 }
 answer5.onclick = nextQuestion4;
+fifincorrect.onclick= timeLoss;
+fifincorrect1.onclick= timeLoss;
+fifincorrect2.onclick= timeLoss;
+
 function saveDetails(){
     var highDetails = {
  highInitials:initials.value.trim(),
  highScore: score
-
-
-    }
-localStorage.setItem('highDetails', JSON.stringify(highDetails))
+ };
+localStorage.setItem('highDetails', JSON.stringify(highDetails));
+renderDetails();
 }
 
 function renderDetails(){
@@ -107,7 +128,7 @@ function renderDetails(){
         else{
             return;
         }
-        console.log( lastGame + score);
+        console.log(highDetails);
 
 }
 submit.addEventListener("click", function(event){
@@ -119,7 +140,7 @@ submit.addEventListener("click", function(event){
 
 })
 
-/*function scoreboard(){
+function scoreboard(){
     enterhighscores.style.display='none';
     alltime.style.display='block';
     saveDetails();
@@ -134,7 +155,7 @@ function homescreen(){
   main.style.display='block';
   
 }
-retbut.onclick = homescreen;*/
+
 
 
 
