93c4e581bea174baa0df518590c433f717baab90
gitkcb <kcharlesbell@gmail.com>
2022-10-11 18:02:55 -0400

    tried to fix bugs- couldn't

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,40 +19,44 @@ var incorrect= document.querySelectorAll('.incorrect');
 var submit = document.querySelector('.submit');
 var retbut= document.querySelector('.return');
 var initials= document.querySelector('#textarea');
-var firincorrect=first.querySelector('.incorrect')
-var firincorrect1=first.querySelector('.incorrect1')
-var firincorrect2=first.querySelector('.incorrect2')
-var secincorrect= second.querySelector('.incorrect')
-var secincorrect1=second.querySelector('.incorrect1')
-var secincorrect2=second.querySelector('.incorrect2')
-var thiincorrect= third.querySelector('.incorrect')
-var thiincorrect1=third.querySelector('.incorrect1')
-var thiincorrect2=third.querySelector('.incorrect2')
-var fouincorrect= fourth.querySelector('.incorrect')
-var fouincorrect1=fourth.querySelector('.incorrect1')
-var fouincorrect2=fourth.querySelector('.incorrect2')
-var fifincorrect= fifth.querySelector('.incorrect')
-var fifincorrect1=fifth.querySelector('.incorrect1')
-var fifincorrect2=fifth.querySelector('.incorrect2')
+var firincorrect=first.querySelector('.incorrect');
+var firincorrect1=first.querySelector('.incorrect1');
+var firincorrect2=first.querySelector('.incorrect2');
+var secincorrect= second.querySelector('.incorrect');
+var secincorrect1=second.querySelector('.incorrect1');
+var secincorrect2=second.querySelector('.incorrect2');
+var thiincorrect= third.querySelector('.incorrect');
+var thiincorrect1=third.querySelector('.incorrect1');
+var thiincorrect2=third.querySelector('.incorrect2');
+var fouincorrect= fourth.querySelector('.incorrect');
+var fouincorrect1=fourth.querySelector('.incorrect1');
+var fouincorrect2=fourth.querySelector('.incorrect2');
+var fifincorrect= fifth.querySelector('.incorrect');
+var fifincorrect1=fifth.querySelector('.incorrect1');
+var fifincorrect2=fifth.querySelector('.incorrect2');
+var goToHighscore=document.querySelector('.left');
+
 
 var timerInterval;
 var secondsLeft = 70;
 function startTimer(){
     welcome.style.display ="none";
     first.style.display ='block'; 
+
     timerInterval = setInterval(function(){
     
     secondsLeft--;
     timeEl.textContent= "Time:" + secondsLeft;
-    },1000)}
+    if (secondsLeft<1){
+        clearInterval(timerInterval);
+        main.innerHTML ='You ran out of time!';
+}
+    },1000)
+    }
 function startQuiz(){
-    startTimer(); } 
-function myStopFunction() {
-    if (secondsLeft===0){
-    clearInterval(timerInterval);
-    main.innerHTML ='You ran out of time!';
-   }}
-   myStopFunction();
+    startTimer(); 
+
+} 
 function timeLoss(){
     secondsLeft -= 10;
 }
@@ -136,7 +140,17 @@ function mainScreen(){
     welcome.style.display='block';
 }
 retbut.onclick = mainScreen;
+function goTo(){
+    alltime.style.display="block";
+    welcome.style.display="none";
+    first.style.display="none";
+    second.style.display="none";
+    third.style.display="none";
+    fourth.style.display="none";
+    fifth.style.display="none";
 
+}
+goToHighscore.onclick = goTo;
 
 
 
